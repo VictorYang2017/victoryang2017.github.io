97c6cd3a931e7564539fd8146f6fe5ab2129dd02
Victor Yang <zonezoneyang@gmail.com>
2018-06-19 23:34:23 +1200

    Add behance api project

---
+++ projects/Projects2018/behance-app/Behance-App/src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import the vue instance
+import Vue from 'vue'
+//import the vue router
+import VueRouter from 'vue-router'
+//import the vue resorce
+import VueResource from 'vue-resource'
+//import the vuefire
+// import VueFire from 'vuefire'
+//import the App component
+import App from './App'
+// import the moment plugin
+import moment from 'moment'
+
+//import Vue Masonry
+import VueMasonry from 'vue-masonry-css'
+
+Vue.use(VueMasonry);
+
+//tell Vue to use the router
+Vue.use(VueRouter)
+//tell Vue to use the resource
+Vue.use(VueResource)
+//tell vue to use the moment
+Vue.use(moment)
+
+//import components
+import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
+import Victor from './components/Photography-Victor/Photography-Victor'
+import VictorPhotographerListPage from './components/Photography-Victor/Photographer-list-page-Victor'
+import VictorPhotographerProfileDetailPage from './components/Photography-Victor/Photographer-profile-detail-page-Victor'
+import VictorPhotographerProjectDetailPage from './components/Photography-Victor/Photographer-project-detail-page-Victor'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/contact', component: Contact },
+  { path: '/photography', component: Victor },
+  { path: '/photographer-list-page', component: VictorPhotographerListPage },
+  { path: '/photographer-profile-detail-page/:individualPhotographerUsername', component: VictorPhotographerProfileDetailPage, props: true },
+  { path: '/photographer-project-detail-page/:individualPhotographerUserId/:individualPhotographerProject', component: VictorPhotographerProjectDetailPage, props: true },
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+//instatiate the vue instance
+new Vue({
+  //define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the Vue instance
+  router
+}).$mount('#app')//mount the router on the app
