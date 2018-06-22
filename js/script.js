53c2fb88c4c75f6a181c5a9bba3072044e5e52e8
Victor Yang <zonezoneyang@gmail.com>
2018-06-22 15:13:49 +1200

    Modified javascript codes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 $(function(){
     function Portfolio(image){
         this.image = image;
+        var self = this;
         this.addFlipClass = function(){
             this.image.addClass('logo-flip');
         }
         this.removeFlipClass = function(){
             this.image.removeClass('logo-flip');
         }
+        this.executeTheFlip = function(){
+            this.image.on({
+                'mouseenter':function(){self.addFlipClass();},
+                'mouseleave':function(){self.removeFlipClass();}
+            });
+        }
     }
     var portfolio = new Portfolio($('#logo-container'));
-
-
-    portfolio.image.on({
-        'mouseenter':function(){portfolio.addFlipClass();},
-        'mouseleave':function(){portfolio.removeFlipClass();}
-    });
+    portfolio.executeTheFlip();
 
 });
