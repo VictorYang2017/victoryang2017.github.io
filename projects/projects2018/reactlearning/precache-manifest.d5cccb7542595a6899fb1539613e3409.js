cc8a59eb36d356bd321271970157bf9f65d99395
Victor Yang <zonezoneyang@gmail.com>
2018-11-30 02:21:15 +1300

    30112018

---
+++ projects/projects2018/reactlearning/precache-manifest.d5cccb7542595a6899fb1539613e3409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4347f9f8d39211fd162c",
+    "url": "/static/js/main.4347f9f8.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "4347f9f8d39211fd162c",
+    "url": "/static/css/main.1e3aa08b.chunk.css"
+  },
+  {
+    "revision": "f5bba199bbfab537a6f42878c48f41e9",
+    "url": "/index.html"
+  }
+];
