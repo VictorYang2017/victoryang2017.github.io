a3f3399e75f279ec0abfe07412b839aebd3e4932
Victor Yang <zonezoneyang@gmail.com>
2020-04-29 23:17:24 +1200

    contents creation

---
+++ projects/projects2018/--testing-temp--/precache-manifest.2f430301ba70a032683358daa124a98e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7f846549b8e373ec0805ca8f293243",
+    "url": "/projects/projects2018/--testing-temp--/index.html"
+  },
+  {
+    "revision": "732f0e5a8fb9cb2c2182",
+    "url": "/projects/projects2018/--testing-temp--/static/css/main.b19f6d9b.chunk.css"
+  },
+  {
+    "revision": "cce5eacfbd074673ee56",
+    "url": "/projects/projects2018/--testing-temp--/static/js/2.812f48d4.chunk.js"
+  },
+  {
+    "revision": "067c720f316f6144a1c66a8687f10ed7",
+    "url": "/projects/projects2018/--testing-temp--/static/js/2.812f48d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732f0e5a8fb9cb2c2182",
+    "url": "/projects/projects2018/--testing-temp--/static/js/main.7dc38f1a.chunk.js"
+  },
+  {
+    "revision": "f79ac3dca1cff82d4730",
+    "url": "/projects/projects2018/--testing-temp--/static/js/runtime-main.db7f8100.js"
+  },
+  {
+    "revision": "0f2e1551cce75d83e7b89178793a367e",
+    "url": "/projects/projects2018/--testing-temp--/static/media/background-pattern.0f2e1551.png"
+  },
+  {
+    "revision": "7a2a35e046f5e3d53a0d4d1882581dac",
+    "url": "/projects/projects2018/--testing-temp--/static/media/cpp-min.7a2a35e0.jpg"
+  },
+  {
+    "revision": "da25e823f55cd361ae275a4ceb9532b9",
+    "url": "/projects/projects2018/--testing-temp--/static/media/cpp.da25e823.jpg"
+  },
+  {
+    "revision": "7ce50c0e1dc383bf194edbcc0e2d7313",
+    "url": "/projects/projects2018/--testing-temp--/static/media/das.7ce50c0e.jpg"
+  },
+  {
+    "revision": "307b61e4d9e5bd970acbed844f4f92b1",
+    "url": "/projects/projects2018/--testing-temp--/static/media/logo.307b61e4.png"
+  },
+  {
+    "revision": "c9c6401bbe422458ea752da24c5837d1",
+    "url": "/projects/projects2018/--testing-temp--/static/media/me-one.c9c6401b.jpg"
+  },
+  {
+    "revision": "fdd872c470aa8d983d871b16b293f115",
+    "url": "/projects/projects2018/--testing-temp--/static/media/me-two.fdd872c4.jpg"
+  },
+  {
+    "revision": "511e94d9a8e81c49a64b7d882f139317",
+    "url": "/projects/projects2018/--testing-temp--/static/media/projectloadingspinner.511e94d9.svg"
+  },
+  {
+    "revision": "09442f8adcdb6cd65f834b0943441f23",
+    "url": "/projects/projects2018/--testing-temp--/static/media/selfie-min.09442f8a.jpg"
+  },
+  {
+    "revision": "63ca5033f2747d2ace6f5ef51723f25d",
+    "url": "/projects/projects2018/--testing-temp--/static/media/selfie.63ca5033.jpg"
+  },
+  {
+    "revision": "7a0617b400b95c176aa1bdf08f43134e",
+    "url": "/projects/projects2018/--testing-temp--/static/media/sorry-bubble.7a0617b4.png"
+  },
+  {
+    "revision": "6b2630d1c69493f4abac46def20b2bc2",
+    "url": "/projects/projects2018/--testing-temp--/static/media/under-construction.6b2630d1.png"
+  }
+]);
